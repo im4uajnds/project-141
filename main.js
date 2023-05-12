efdae42c42550a526a419d309ea5c74178944edb
im4uajnds <53973675+im4uajnds@users.noreply.github.com>
2023-05-12 21:21:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,109 @@
+rightwristX = "";
+rightwristY = "";
+rightwristSCORE = "";
+/*created by prashant shukla */
+
+var paddle2 =10,paddle1=10;
+
+var paddle1X = 10,paddle1Height = 110;
+var paddle2Y = 685,paddle2Height = 70;
+
+var score1 = 0, score2 =0;
+var paddle1Y;
+
+var  playerscore =0;
+var audio1;
+var pcscore =0;
+//ball x and y and speedx speed y and radius
+var ball = {
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
+}
+
+function setup(){
+  var canvas = createCanvas(700,600);
+  canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+
+function draw(){
+ background(0); 
+ image(video,0,0,700,600);
+ 
+ fill("black");
+ stroke("black");
+ rect(680,0,20,700);
+
+ fill("black");
+ stroke("black");
+ rect(0,0,20,700);
+ 
+   //funtion paddleInCanvas call 
+   paddleInCanvas();
+ 
+   //left paddle
+   fill(250,0,0);
+    stroke(0,0,250);
+    strokeWeight(0.5);
+   paddle1Y = mouseY; 
+   rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+   
+   
+    //pc computer paddle
+    fill("#FFA500");
+    stroke("#FFA500");
+   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+    
+    //function midline call
+    midline();
+    
+    //funtion drawScore call 
+   drawScore();
+   
+   //function models call  
+   models();
+   
+   //function move call which in very important
+    move();
+    if(rightwristSCORE > 0.2){
+      fill("#ff0000");
+      stroke("#ff0000");
+      circle(rightwristX,rightwristY,20);
+    }
+}
+
+
+
+//function reset when ball does notcame in the contact of padde
+function reset(){
+   ball.x = width/2+100,
+   ball.y = height/2+100;
+   ball.dx=3;
+   ball.dy =3;
+   
+}
+
+
+//function midline draw a line in center
+function midline(){
+    for(i=0;i<480;i+=10) {
+    var y = 0;
+    fill("white");
+    stroke(0);
+    rect(width/2,y+i,10,480);
+    }
+}
+
+
+//function drawScore show scores
+function drawScore(){
+    textAlign(CENTER);
+    textSize(20);
